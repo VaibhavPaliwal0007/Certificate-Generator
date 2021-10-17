0dba3b32d5a2503bb0dee7bd46e364ed9817fa3c
VaibhavPaliwal0007 <78612380+VaibhavPaliwal0007@users.noreply.github.com>
2021-10-17 15:18:58 +0530

    added without saving to database code

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const home = require('./routes/home')
-require('./Database/db')
+// require('./Database/db')
 
 const app = express()
 const port = 8000
 
 app.use(express.json())
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
 app.use(home)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
--- Server/routes/home.js
+++ Server/routes/home.js
@@ -10,45 +10,61 @@ router.get('/v1', (req, res) => {
     res.send("Hello World")
 })
 
-const upload = multer({
-    limits: {
-       fileSize: 1000000
-    },
+// const storage = multer.diskStorage({
+//     destination: function(req, file, cb){
+//         cb(null, '../uploads')
+//     },
 
-    fileFilter(req, file, cb) {
-        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
-            return cb(new Error('Please upload an image'))
-        }
+//     filename: function(req, file, cb) {
+//         cb(null, file.originalname)
+//     },
+// })
 
-    //     if(file.originalname.match(/\.(jpg|jpeg)$/)){
-    //         try{
-    //             let convertedFile = file.originalname.replace(/\.(jpg|jpeg)$/, '.png')
-    //             const File = await Jimp.read(file.originalname)
-
-    //             return await File.write(convertedFile)
-    //         }
-            
-    //         catch(e){
-    //             return console.log(e)
-    //         }
-    //     }
-
-        cb(undefined, true)
+const fileFilter = (req, file, cb) => {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|webp)$/)){
+        return cb(new Error('Please upload an image'), false)
     }
-})
+
+//     if(file.originalname.match(/\.(jpg|jpeg)$/)){
+//         try{
+//             let convertedFile = file.originalname.replace(/\.(jpg|jpeg)$/, '.png')
+//             const File = await Jimp.read(file.originalname)
+
+//             return await File.write(convertedFile)
+//         }
+        
+//         catch(e){
+//             return console.log(e)
+//         }
+//     }
+
+    cb(undefined, true)
+}
+
+const upload = multer({ 
+    limits: {
+    fileSize: 1024 * 1024 * 10
+    },
+    fileFilter,
+    // storage
+ })
 
 router.post('/v1/png', upload.single('certificate'), async(req, res) => {
-       const buffer = await sharp(req.file.buffer).png().toBuffer()
-       const certificate =  new Png({
-           name: req.file.originalname,
-           certificate: buffer
-        })
+        const buffer = await sharp(req.file.buffer).png().toBuffer()
+    //    const png =  new Png({
+    //        name: req.file.originalname,
+    //        certificate: buffer
+    //     })
+       
+    //    await console.log(req.file)
+       
+    //    await certificate.save()
 
-       await certificate.save()
-       res.status(200).send({ certificate })
+       res.set('Content-Type', 'image/png')
+       res.status(200).send(buffer)
 
 }, (error, req, res, next) => {
-    res.status(400).send({ error : error.message })
+    res.status(400).send({ error, req  })
 })
 
 router.get('/v1/getpng', async(req, res) => {
